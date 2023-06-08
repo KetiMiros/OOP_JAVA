c9f4d09d59ace3fc687d886f39f5954964c2a718
Ketrin_Miros <miroslavskayaketi@gmail.com>
2023-06-08 16:46:02 +0300

    Update OOP_JAVA

---
+++ srs/HW_OOP/Main.java
@@ -0,0 +1,32 @@
+package HW_OOP.HW1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    Product product1 = new Product();
+    product1.name = "Nestle";
+    product1.callory = 300.50;
+
+    Product product3 = new Product();
+
+    Product product4 = new AlpenGold("Шоколад 100", 300);
+    Product product5 = new AlpenGold("Шоколад 300", 900);
+    Product product6 = new AlpenGold("Шоколад 400", 1100);
+    List<Product> products = new ArrayList<>();
+
+    products.add(product5);
+    products.add(product4);
+
+    VendingMachine vendingMachine = new VendingMachine(products);
+
+    AlpenGold chocolateRes = vendingMachine.getChocolate("Шоколад 100", 300);
+    if (chocolateRes != null) {
+      System.out.println("Вы купили: ");
+      System.out.println(chocolateRes.displayInfo());
+    } else {
+      System.out.println("Такой шоколадки нет в автомате");
+    }
+  }
+}
---
+++ srs/HW_OOP/Product.java
@@ -0,0 +1,93 @@
+// package HW_OOP.HW1;
+
+public class Product {
+  String name;
+  double callory;
+
+  String param_name;
+  Double param_callory;
+  int param_price;
+
+  /**
+   * создаем два метода геттер(акцессер - получать доступ) и сеттер(мьютейтер -
+   * изменять)
+   * они нужны для создания обёртки над нашим методом в виде поля
+   */
+  public double getCallory() {
+    return callory;
+  }
+
+  public void setCallory(double Callory) {
+    if (Callory <= 0)
+      throw new RuntimeException("Некорректно введено кол-во каллорий");
+    this.callory = Callory;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * вызов секции инициализатора наших параметров
+   * он отрабатывает первее наших конструкторов
+   */
+  {
+    System.out.println("Initializator");
+    param_callory = 400.0;
+    param_name = "Name";
+    param_price = 123;
+  }
+
+  /**
+   * Пустой конструктор - БАЗОВЫЙ
+   */
+  public Product() {
+    name = "Продукт";
+    callory = 1.0;
+  }
+
+  /**
+   * Конструктор с 1 параметром - Название продукта
+   * 
+   * @param inputName
+   */
+  public Product(String inputName) {
+    name = inputName;
+  }
+
+  /**
+   * Конструктор с 2 параметрами - Название продукта и кол-во каллорий
+   * 
+   * @param inputName
+   */
+  public Product(String inputName, double inputCallory) {
+    /**
+     * здесь прописываем исключения которые поможет пользователю не сломать нашу
+     * программу
+     */
+    if (inputName.length() < 3)
+      this.name = "Name";
+    else
+      this.name = inputName;
+    // if (inputCallory<=50)
+    // inputCallory = 100;
+    // this.callory = inputCallory;
+    if (inputCallory <= 50)
+      throw new RuntimeException("Некорректно введено кол-во каллорий");
+    this.callory = inputCallory;
+  }
+
+  /**
+   * вспомогательный метод возвращает информацию по объекту
+   * 
+   * @return Информация по продукту
+   */
+  String displayInfo() {
+    return String.format("%s - %f", name, callory);
+
+  }
+}
